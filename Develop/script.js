5efe379caa0caf2822afbb7dcd47dd19a049df3f
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-05-28 16:11:53 -0400

    More testing, still working through console errors

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
-let generateBtn = document.querySelector("#generate");
-// let clipboard = new clipboard(".copy");
+let clipboard = new clipboard(".copy");
 let lowercase = "abcdefghijklmnopqrstuvwxyz",
   uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers = "0123456789",
@@ -7,11 +6,11 @@ let lowercase = "abcdefghijklmnopqrstuvwxyz",
   lowercaseInput = document.getElementById("lowercase"),
   uppercaseInput = document.getElementById("uppercase"),
   numbersInput = document.getElementById("numbers"),
-  specialCharInput = document.getElementById("specialChar");
-lengthInput = document.getElementById("length");
-passwordField = document.getElementById("pass-field");
-generateBtn = document.getElementById("generate");
-copyButton = document.getElementById("copy");
+  specialCharInput = document.getElementById("specialChar"),
+  lengthInput = document.getElementById("length"),
+  passwordField = document.getElementById("pass-field"),
+  generateBtn = document.getElementById("generate"),
+  copyButton = document.getElementById("copy");
 
 function writePassword() {
   userPassword = "";
@@ -29,3 +28,28 @@ function writePassword() {
     passwordCharSet += numbers;
   }
 }
+
+clipboard.on("success", function (e) {
+  console.info("Action:", e.action);
+  console.info("Text:", e.text);
+  console.info("Trigger:", e.trigger);
+  let alertbox = document.getElementById("alert");
+  alertbox.innerHTML = "Copied!";
+  alertbox.classList.add("success");
+  setTimeout(function () {
+    alertbox.classList.remove("success");
+  }, 3000);
+
+  e.clearSelection();
+});
+
+clipboard.on("error", function (e) {
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
+  let alertbox = document.getElementById("alert");
+  alertbox.innerHTML = "Try select the text to copy";
+  alertbox.classList.add("fail");
+  setTimeout(function () {
+    alertbox.classList.remove("fail");
+  }, 3000);
+});
